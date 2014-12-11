e36d0f94723b43c5418166ce76f5f17a8a8ce54e
Amanda Murray <greenhatgod@gmail.com>
2014-12-11 13:10:37 -0500

    Is it break yet

---
+++ game_v.1/Assets/what needs to be done.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whatneedstobedone : MonoBehaviour {
+
+	//I am working really hard on this game! I am trying to make something happen when the player gets hurt, and also a way to score. Then when that's over we can get a start screen and 
+	//a game over screen. 
+}
